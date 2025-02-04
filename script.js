11ea1a394b5f96876ad71611da5817839c896ebf
Juliana Thompson <jgtbandeira@gmail.com>
2025-02-04 20:05:54 -0300

    alterar linguagem

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let tentativas = 1
 function exibirTexto(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto
-  responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate:1.2})
+  responsiveVoice.speak(texto, "UK English Female", {rate:1.2})
 };
 
 function exibirMensagemInicial() {
